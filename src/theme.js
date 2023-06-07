ed9e2e9823a238b16125defcdbc798ea80e3bc4d
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-06-08 00:46:21 +0100

    theme.js added

---
+++ src/theme.js
@@ -0,0 +1,21 @@
+export const theme = {
+    color: {
+        lightgray: "#d3d3d3",
+        red: "#ff0000",
+        teal: "#008080",
+        white: "#ffffff",
+        darkcyan: "#008b8b",
+        lightseagreen: "#20b2aa",
+        gainsboro: "#dcdcdc",
+        transparent: "rgba(0,0,0,0)",
+        forestgreen: "#228c22",
+        forestgreenHover: "#27a027",
+        forestgreenActive: "#2cb52c",
+        amaranth: "#db143c",
+        amaranthHover: "#eb2d53",
+        amaranthActive: "#ef5272",
+    },
+    breakpoint: {
+        mobileMax: 500,
+    },
+};
