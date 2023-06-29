0ceb4727a6b9cbc07029013b38fccc9591e27aee
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-06-29 23:33:28 +0100

    redux  + react started

---
+++ src/features/tasks/Tasks.js
@@ -0,0 +1,47 @@
+import Form from "./Form";
+import TaskList from "./TaskList";
+import Buttons from "./SectionButtons";
+import Section from "../../common/Section";
+import Header from "../../common/Header";
+import Container from "../../common/Container";
+import { useTasks } from "../../useTasks";
+
+function Tasks() {
+  const {
+    tasks,
+    hideDone,
+    toggleHideDone,
+    removeTask,
+    toggleTaskDone,
+    setAllDone,
+    addNewTask,
+  } = useTasks();
+
+  return (
+    <Container>
+      <Header title="Task list" />
+      <Section title="Add a new task" body={<Form addNewTask={addNewTask} />} />
+      <Section
+        title="Your tasks"
+        body={
+          <TaskList
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
+        }
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
+          />
+        }
+      />
+    </Container>
+  );
+}
+
+export default Tasks;
