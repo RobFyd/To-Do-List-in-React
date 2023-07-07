ac36116819189b48b1f2ed78d4a20020fc96ef52
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-07-07 23:39:05 +0100

    Tasks.js upgraded

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -7,23 +7,16 @@ import Header from "../../common/Header";
 import Container from "../../common/Container";
 import { useTasks } from "../../useTasks";
 import { selectTasks } from "./tasksSlice";
-import { useState } from "react";
 
 function Tasks() {
-  const { tasks } = useSelector(selectTasks);
-
-  const { removeTask, toggleTaskDone, setAllDone, addNewTask } = useTasks();
-
   return (
     <Container>
       <Header title="Task list" />
-      <Section title="Add a new task" body={<Form addNewTask={addNewTask} />} />
+      <Section title="Add a new task" body={<Form />} />
       <Section
         title="Your tasks"
-        body={
-          <TaskList removeTask={removeTask} toggleTaskDone={toggleTaskDone} />
-        }
-        extraHeaderContent={<Buttons setAllDone={setAllDone} />}
+        body={<TaskList />}
+        extraHeaderContent={<Buttons />}
       />
     </Container>
   );
