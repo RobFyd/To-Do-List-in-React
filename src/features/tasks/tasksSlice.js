ba990c277d4ed418c52a65b6feecf981363d8721
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-07-11 23:44:25 +0100

    payloads named

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -7,18 +7,18 @@ const tasksSlice = createSlice({
     hideDone: false,
   },
   reducers: {
-    addTask: ({ tasks }, { payload }) => {
-      tasks.push(payload);
+    addTask: ({ tasks }, { payload: task }) => {
+      tasks.push(task);
     },
     toggleHideDone: (state) => {
       state.hideDone = !state.hideDone;
     },
-    toggleTaskDone: ({ tasks }, { payload }) => {
-      const index = tasks.findIndex(({ id }) => id === payload);
+    toggleTaskDone: ({ tasks }, { payload: taskID }) => {
+      const index = tasks.findIndex(({ id }) => id === taskID);
       tasks[index].done = !tasks[index].done;
     },
-    removeTask: ({ tasks }, { payload }) => {
-      const index = tasks.findIndex(({ id }) => id === payload);
+    removeTask: ({ tasks }, { payload: taskID  }) => {
+      const index = tasks.findIndex(({ id }) => id === taskID);
       tasks.splice(index, 1);
     },
     setAllDone: ({ tasks }) => {
@@ -32,6 +32,6 @@ const tasksSlice = createSlice({
 export const { addTask, toggleHideDone, toggleTaskDone, removeTask, setAllDone } =
   tasksSlice.actions;
 
-export const selectTasks = (state) => state.tasks;
+export const selectTasks = state => selectTasksState(state).tasks;
 
 export default tasksSlice.reducer;
