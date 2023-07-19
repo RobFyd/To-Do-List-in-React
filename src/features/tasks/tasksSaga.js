fba70bb5834f5adaa347f276d254cb1fc7d04245
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-07-20 00:45:04 +0100

    tasksLocalStorage.js added

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -12,6 +12,5 @@ function* fetchExampleTasksHandler() {
 }
 
 export function* watchFetchExampleTasks() {
-    console.log("Saga is connected");
   yield takeEvery(fetchExampleTasks.type, fetchExampleTasksHandler);
 }
