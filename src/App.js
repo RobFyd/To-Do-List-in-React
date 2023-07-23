687aade8eb44fc87c9d54a5ce02a3021a74efd63
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-07-23 21:31:06 +0100

    link and subpage "about author" added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Link,
-} from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import Tasks from "./features/tasks/Tasks";
+import Author from "./features/author/Author";
 
 // "react-router-dom/cjs/react-router-dom.min"
 
@@ -15,10 +11,16 @@ const App = () => (
         <li>
           <Link to="/tasksList">Tasks</Link>
         </li>
+        <li>
+          <Link to="/author">About author</Link>
+        </li>
       </ul>
       <Switch>
         <Route path="/tasksList">
           <Tasks />
+        </Route>
+        <Route path="/author">
+          <Author />
         </Route>
       </Switch>
     </nav>
