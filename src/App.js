046f7584dc5035638729b96eac6264584115df41
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-07-23 21:13:35 +0100

    author subpage added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 import Tasks from "./features/tasks/Tasks";
 
-const App = () => <Tasks />;
+// "react-router-dom/cjs/react-router-dom.min"
+
+const App = () => (
+  <BrowserRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/tasksList">Tasks</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/tasksList">
+          <Tasks />
+        </Route>
+      </Switch>
+    </nav>
+  </BrowserRouter>
+);
 
 export default App;
