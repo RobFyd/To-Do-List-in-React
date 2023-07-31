6d688906e73540c5646dc9af6fe1a75a34f7e1b9
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-07-31 23:44:33 +0100

    navLink added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import { HashRouter, Switch, Route, NavLink, Redirect } from "react-router-dom";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import TasksPage from "./features/tasks/TasksPage";
 import TaskPage from "./features/tasks/TaskPage";
 import AuthorPage from "./features/author/AuthorPage";
+import { StyledNavLink } from "./styled";
 
 const App = () => (
   <HashRouter>
     <nav>
       <ul>
         <li>
-          <NavLink activeClassName="active" to="/tasksList">
-            Tasks
-          </NavLink>
+          <StyledNavLink to="/tasksList">Tasks</StyledNavLink>
         </li>
         <li>
-          <NavLink activeClassName="active" to="/author">
-            About author
-          </NavLink>
+          <StyledNavLink to="/author">About author</StyledNavLink>
         </li>
       </ul>
       <Switch>
