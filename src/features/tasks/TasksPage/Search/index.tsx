e7dc575200b55b6587c9705bd9109f742d5af5f9
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-03-30 21:42:24 +0000

    Search to TS refactored

--- src/features/tasks/TasksPage/Search/index.tsx
+++ src/features/tasks/TasksPage/Search/index.tsx
@@ -5,11 +5,17 @@ import {
   useReplaceQueryParameter,
 } from "../queryParameters";
 
+interface OnInputChangeProps {
+  target: {
+    value: string;
+  };
+}
+
 const Search = () => {
   const query = useQueryParameter(searchQueryParamName);
   const replaceQueryParameter = useReplaceQueryParameter();
 
-  const onInputChange = ({ target }) => {
+  const onInputChange = ({ target }: OnInputChangeProps) => {
     replaceQueryParameter({
       key: searchQueryParamName,
       value: target.value.trim() !== "" ? target.value : undefined,
@@ -19,7 +25,7 @@ const Search = () => {
   return (
     <TasksFormSearch>
       <Input
-        margin
+        margin="0 0 20px"
         placeholder="Filter tasks"
         value={query || ""}
         onChange={onInputChange}
